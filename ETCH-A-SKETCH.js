125e8e89e6e311a3487558d3500857e85746f033
akashyadav <akashyadav256526@gmail.com>
2019-04-02 15:57:44 +0530

    removed all console.log from layout version

--- ETCH-A-SKETCH.js
+++ ETCH-A-SKETCH.js
@@ -10,17 +10,14 @@ container.style.gridTemplateColumns=`repeat(${grid_column},1fr	)`;
 //add the div element in the grid 
 for(i=1;i<=grid_column*grid_column;i++){ 
   divRows=document.createElement('div');
-  let x = document.createTextNode(i);
-  divRows.appendChild(x);
   divRows.classList.add('rows');
   divRows.setAttribute('id',`ele${i}`);
   divRows.style.height=600/grid_column;
   divRows.style.width=960/grid_column;
   container.appendChild(divRows);
 }
-console.log(i);
  //round of the corners of corner div in grid
- document.querySelector('#ele1').style.borderRadius='60px 0 0 0';
- document.querySelector(`#ele${grid_column}`).style.borderRadius='0 60px 0 0';
- document.querySelector(`#ele${i-grid_column}`).style.borderRadius='0 0 0 60px';
- document.querySelector(`#ele${i-1}`).style.borderRadius='0 0 60px 0';
+ document.querySelector('#ele1').style.borderRadius='25px 0 0 0';
+ document.querySelector(`#ele${grid_column}`).style.borderRadius='0 25px 0 0';
+ document.querySelector(`#ele${i-grid_column}`).style.borderRadius='0 0 0 25px';
+ document.querySelector(`#ele${i-1}`).style.borderRadius='0 0 25px 0';
