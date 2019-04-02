7dd0fb8e145b7552fc41914c87e68c3d6a689fd3
akashyadav <akashyadav256526@gmail.com>
2019-04-02 15:47:02 +0530

    layout done

--- ETCH-A-SKETCH.js
+++ ETCH-A-SKETCH.js
@@ -1,14 +1,26 @@
 //jshint esversion:6
+//select grid container
 let container=document.querySelector('.container');
+//initilize the object for div
 let divRows=0;
+//ask for grid dimension
 let grid_column=prompt('enter');
+//set the number of column for grid
 container.style.gridTemplateColumns=`repeat(${grid_column},1fr	)`;
-for(i=0;i<grid_column*grid_column;i++)
-{ console.log('worked')
+//add the div element in the grid 
+for(i=1;i<=grid_column*grid_column;i++){ 
   divRows=document.createElement('div');
-  let text=document.createTextNode(i);
+  let x = document.createTextNode(i);
+  divRows.appendChild(x);
   divRows.classList.add('rows');
-  divRows.appendChild(text);	
+  divRows.setAttribute('id',`ele${i}`);
+  divRows.style.height=600/grid_column;
+  divRows.style.width=960/grid_column;
   container.appendChild(divRows);
 }
-
+console.log(i);
+ //round of the corners of corner div in grid
+ document.querySelector('#ele1').style.borderRadius='60px 0 0 0';
+ document.querySelector(`#ele${grid_column}`).style.borderRadius='0 60px 0 0';
+ document.querySelector(`#ele${i-grid_column}`).style.borderRadius='0 0 0 60px';
+ document.querySelector(`#ele${i-1}`).style.borderRadius='0 0 60px 0';
